15957068be73b59ab3b7a4f89e4ddb3e6329a8d3
Max Garret <max.garret@wanadoo.fr>
2015-11-18 19:26:06 +0100

    Major Fixes
    Core rewritten for better lisibility and modularity (damn it was ugly
    before...)
    Fixed an issue with the number of the shortcut being written anyway
    Fixed an issue where the shortcuts would no longer work
    Changed some files organisation

--- mailPaste.py
+++ mailPaste.py
@@ -2,81 +2,21 @@
 
 import pyhk, pyperclip
 import sys, time
-import win32com.client
+import win32com.client, pythoncom
 import codecs
 from Tkinter import * 
 from ttk import *
 
-
-def f0():
-	#print ("Mail 0")
-	shell = win32com.client.Dispatch("WScript.Shell")
-	pyperclip.copy(open("mail0.txt").read())
-	shell.SendKeys("^v")
-	#del_last(shell)
-	
-def f1():
-	#print ("Mail 1")
-	shell = win32com.client.Dispatch("WScript.Shell")
-	pyperclip.copy(open("mail1.txt").read())
-	shell.SendKeys("^v")
-	#del_last(shell)
-	
-def f2():
-	#print ("Mail 2")
-	shell = win32com.client.Dispatch("WScript.Shell")
-	pyperclip.copy(open("mail2.txt").read())
-	shell.SendKeys("^v")
-	#del_last(shell)
+class funWrap:
+	def __init__(self, num):
+		self.number = num
 		
-def f3():
-	#print ("Mail 3")
-	shell = win32com.client.Dispatch("WScript.Shell")
-	pyperclip.copy(open("mail3.txt").read())
-	shell.SendKeys("^v")
-	#del_last(shell)
-		
-def f4():
-	#print ("Mail 4")
-	shell = win32com.client.Dispatch("WScript.Shell")
-	pyperclip.copy(open("mail4.txt").read())
-	shell.SendKeys("^v")
-	#del_last(shell)
-		
-def f5():
-	#print ("Mail 5")
-	shell = win32com.client.Dispatch("WScript.Shell")
-	pyperclip.copy(open("mail5.txt").read())
-	shell.SendKeys("^v")
-	#del_last(shell)
-		
-def f6():
-	#print ("Mail 6")
-	shell = win32com.client.Dispatch("WScript.Shell")
-	pyperclip.copy(open("mail6.txt").read())
-	shell.SendKeys("^v")
-	#del_last(shell)	
-	
-def f7():
-	#print ("Mail 7")
-	shell = win32com.client.Dispatch("WScript.Shell")
-	pyperclip.copy(open("mail7.txt").read())
-	shell.SendKeys("^v")	
-	#del_last(shell)
-	
-def f8():
-	#print ("Mail 8")
-	shell = win32com.client.Dispatch("WScript.Shell")
-	pyperclip.copy(open("mail8.txt").read())
-	shell.SendKeys("^v")
-	#del_last(shell)	
-	
-def f9():
-	#print ("Mail 9")
-	shell = win32com.client.Dispatch("WScript.Shell")
-	pyperclip.copy(open("mail9.txt").read())
-	shell.SendKeys("^v")
-	#del_last(shell)
+	def fun(self):
+		i = self.number
+		pythoncom.CoInitialize ()
+		shell = win32com.client.Dispatch("WScript.Shell")
+		pyperclip.copy(open("mail"+str(i)+".txt").read())
+		shell.SendKeys("^v")
 
 def quit():
 	hot.end()
@@ -84,7 +24,7 @@ def quit():
 	root.quit()
 	
 def instructions():
-	s= open("Instructions.txt").read().decode('utf-8')
+	s= open("Python.2.7.10\Instructions.txt").read().decode('utf-8')
 	ins = Toplevel(fenetre)
 	ins.title("MailPaste - Instructions")
 	label = Label(ins, text=s, justify = LEFT)
@@ -100,17 +40,10 @@ def instructions():
 
 #create pyhk class instance
 hot = pyhk.pyhk()
-key = 'Ctrl'
-hot.addHotkey([key,'0'],f0)
-hot.addHotkey([key,'1'],f1)
-hot.addHotkey([key,'2'],f2)
-hot.addHotkey([key,'3'],f3)
-hot.addHotkey([key,'4'],f4)
-hot.addHotkey([key,'5'],f5)
-hot.addHotkey([key,'6'],f6)
-hot.addHotkey([key,'7'],f7)
-hot.addHotkey([key,'8'],f8)
-hot.addHotkey([key,'9'],f9)
+key = 'Lcontrol'
+for i in range(9):
+	t = funWrap(i)
+	hot.addHotkey([key, str(i)], t.fun, isThread = True)
 
 
 #graphical part
--- start.cmd
+++ start.cmd
@@ -1,4 +1,3 @@
 @echo off
 cd %~dp0
-chcp 1252
 start Python.2.7.10\pythonw.exe mailPaste.py %*
